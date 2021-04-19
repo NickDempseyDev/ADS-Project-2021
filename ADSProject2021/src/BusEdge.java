1652c2f7026bfb6f4e1c220594ae19ff6a30ffd1
GuzowskD <guzowskd@tcd.ie>
2021-04-19 15:06:29 +0100

    All worked to implement Bus Edges, finished loading in files, implemented TST.

---
+++ ADSProject2021/src/BusEdge.java
@@ -0,0 +1,135 @@
+public class BusEdge implements Comparable<BusEdge> {
+    private boolean isTransfer;
+    private int cost;
+    private int tripID;
+    private BusStop from;
+    private BusStop to;
+    private String arrivalTime;
+    private String departureTime;
+    private int stopSequence;
+    private double distance;
+
+    // Constructor for transfer type edges
+    public BusEdge(BusStop from, BusStop to, int cost) {
+        isTransfer = true;
+        this.from = from;
+        this.to = to;
+        this.cost = cost / 100;
+
+        tripID = -1;
+        departureTime = "";
+        arrivalTime = "";
+        stopSequence = -1;
+        distance = -1;
+    }
+
+    // Constructor for stop times edges
+    public BusEdge(int tripID, BusStop from, BusStop to, String departureTime, String arrivalTime, int stopSequence,
+            double distance) {
+        isTransfer = false;
+        cost = 1;
+        this.tripID = tripID;
+        this.from = from;
+        this.to = to;
+        this.departureTime = departureTime;
+        this.arrivalTime = arrivalTime;
+        this.stopSequence = stopSequence;
+        this.distance = distance;
+    }
+
+    /**
+     * Returns data of edge as a string
+     * 
+     * @return string of edge data
+     */
+    @Override
+    public String toString() {
+        if (isTransfer) {
+            String s = "TRANSFER From: " + from + ", To: " + to + ", COST: " + cost;
+            return s;
+        } else {
+            String s = "TripID: " + tripID + ", From: " + from.getName() + ", To: " + to.getName() + ", Departue Time: "
+                    + departureTime + ", Arrival Time: " + arrivalTime + ", Seq. No: " + stopSequence + ", Distance: "
+                    + distance + ", COST: " + cost;
+            return s;
+        }
+    }
+
+    /**
+     * Compares edge to input edge based on tripid
+     * 
+     * @param edge: edge to be compared to
+     * @return 1 if >, -1 if <, 0 if equal
+     */
+    @Override
+    public int compareTo(BusEdge edge) {
+        if (tripID < edge.tripID)
+            return -1;
+        else if (tripID > edge.tripID)
+            return 1;
+        return 0;
+    }
+
+    /**
+     * @return edge tripid
+     */
+    public int getTripID() {
+        return tripID;
+    }
+
+    /**
+     * @return edge cost
+     */
+    public int getCost() {
+        return cost;
+    }
+
+    /**
+     * @return edge start busstop
+     */
+    public BusStop getFrom() {
+        return from;
+    }
+
+    /**
+     * @return edge end busstop
+     */
+    public BusStop getTo() {
+        return to;
+    }
+
+    /**
+     * @return edge arrival time
+     */
+    public String getArrivalTime() {
+        return arrivalTime;
+    }
+
+    /**
+     * @return edge departuretime
+     */
+    public String getDepartureTime() {
+        return departureTime;
+    }
+
+    /**
+     * @return edge distance
+     */
+    public double getDistance() {
+        return distance;
+    }
+
+    /**
+     * @return edge seq number
+     */
+    public double getSequenceNumber() {
+        return stopSequence;
+    }
+
+    /**
+     * @return true if it is a transfer, false otherwise
+     */
+    public boolean getIsTransfer() {
+        return isTransfer;
+    }
+}
--- ADSProject2021/src/BusStop.java
+++ ADSProject2021/src/BusStop.java
@@ -1,13 +1,18 @@
-public class BusStop implements Comparable <BusStop>{
-    int ID;
-    int code;
-    String name;
-    String desc;
-    double lat;
-    double lon;
-    String zoneID;
-    int locType;
-    int parent;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class BusStop implements Comparable<BusStop> {
+    private int ID;
+    private int code;
+    private String name;
+    private String desc;
+    private double lat;
+    private double lon;
+    private String zoneID;
+    private int locType;
+    private int parent;
+
+    private ArrayList<BusEdge> edges;
 
     public BusStop(int ID, int code, String name, String desc, double lat, double lon, String zoneID, int locType,
             int parent) {
@@ -20,23 +25,121 @@ public class BusStop implements Comparable <BusStop>{
         this.zoneID = zoneID;
         this.locType = locType;
         this.parent = parent;
+
+        edges = new ArrayList<BusEdge>();
     }
 
+    /**
+     * Returns data of bus stop as a string
+     * 
+     * @return string of bus stop data
+     */
     @Override
     public String toString() {
         return ID + "," + code + "," + name + "," + desc + "," + lat + "," + lon + "," + zoneID + "," + locType + ","
                 + parent;
     }
 
+    /**
+     * Compares bus stop to another busstop based on ids
+     * 
+     * @param stop: bus stop to be compared to
+     * @return 1 if >, -1 if <, 0 if equal
+     */
     @Override
     public int compareTo(BusStop stop) {
         if (this.ID == ((BusStop) stop).ID) {
             return 0;
-        }
-        else if (this.ID < ((BusStop) stop).ID) {
+        } else if (this.ID < ((BusStop) stop).ID) {
             return -1;
         }
         return 1;
     }
 
+    /**
+     * @return bus stop id
+     */
+    public int getID() {
+        return ID;
+    }
+
+    /**
+     * @return bus stop code
+     */
+    public int getCode() {
+        return code;
+    }
+
+    /**
+     * @return bus stop name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @return bus stop desc
+     */
+    public String getDesc() {
+        return desc;
+    }
+
+    /**
+     * @return bus stop lat
+     */
+    public Double getLat() {
+        return lat;
+    }
+
+    /**
+     * @return bus stop lon
+     */
+    public Double getLon() {
+        return lon;
+    }
+
+    /**
+     * @return bus stop zoneID
+     */
+    public String getZoneID() {
+        return zoneID;
+    }
+
+    /**
+     * @return bus stop loctype
+     */
+    public int getLocType() {
+        return locType;
+    }
+
+    /**
+     * @return bus stop parent
+     */
+    public int getParent() {
+        return parent;
+    }
+
+    /**
+     * @return bus stop edges
+     */
+    public ArrayList<BusEdge> getEdges() {
+        return edges;
+    }
+
+    /**
+     * Add edge to the list of edges
+     * 
+     * @param edge: edge to be added to bus stop
+     * @return true if it worked, false otherwise
+     */
+    public boolean addEdge(BusEdge edge) {
+        return edges.add(edge);
+    }
+
+    /**
+     * Sort the edge arraylist
+     */
+    public void sortEdges() {
+        Collections.sort(edges);
+    }
 }
